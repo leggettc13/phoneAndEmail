bacb6b0878d3acbe1578a829d72b33afe1d49ad7
Christopher Leggett <leggettc13@protonmail.com>
2018-05-16 19:48:20 -0400

    Initial Commit

---
+++ phoneAndEmail.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+# phoneAndEmail.py - A program that gets text off of the clipboard, Finds
+# all the phone numbers and email addresses in the text, and then pastes
+# them onto the clipboard.
+
+import pyperclip, re
+
+# TODO: Create Phone Number regex.
+
+# TODO: Create Email regex.
+
+# TODO: Find matches in clipboard text.
+
+# TODO: Copy results to the clipboard.
