d4b367d9bcadfa856e58e67c8cd6d86ab535b346
Christopher Leggett <leggettc13@protonmail.com>
2018-05-16 23:30:09 -0400

    Adds functionality to print results to terminal and copy them to the clipboard

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -33,4 +33,10 @@ for groups in phoneRegex.findall(text):
 for groups in emailRegex.findall(text):
     matches.append(groups[0])
 
-# TODO: Copy results to the clipboard.
+# Copy results to the clipboard
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
