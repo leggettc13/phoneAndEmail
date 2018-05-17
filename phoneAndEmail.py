cb700bbaf3f9f893e44267b0d67914c83ac3df8e
Christopher Leggett <leggettc13@protonmail.com>
2018-05-16 20:17:41 -0400

    Fixes bug in phone number regex

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -14,8 +14,10 @@ phoneRegex = re.compile(r'''(
         (\d{3})                         # first 3 digits
         (\s|-|\.)                       # separator
         (\d{4})                         # last 4 digits
-        (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension
+        (\s*(ext|x|ext\.)\s*(\d{2,5}))?  # extension
         )''', re.VERBOSE)
+
+print(phoneRegex.search("(252) 296-6281 ext. 24").group())
 
 # TODO: Create Email regex.
 
